7ba9241af7575fd6389f0f7d8fe7e8050902522d
ST-Devops <tsubravel@gmail.com>
2026-05-04 14:58:17 +0530

    Add availability zone data source and update subnet resources in VPC module

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,3 +1,7 @@
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
 resource "aws_vpc" "this" {
   cidr_block = "10.0.0.0/16"
 }
@@ -10,13 +14,15 @@ resource "aws_subnet" "public" {
   count                   = 2
   vpc_id                  = aws_vpc.this.id
   cidr_block              = cidrsubnet("10.0.0.0/16", 8, count.index)
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
   map_public_ip_on_launch = true
 }
 
 resource "aws_subnet" "private" {
-  count      = 2
-  vpc_id     = aws_vpc.this.id
-  cidr_block = cidrsubnet("10.0.0.0/16", 8, count.index + 10)
+  count             = 2
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = cidrsubnet("10.0.0.0/16", 8, count.index + 10)
+  availability_zone = data.aws_availability_zones.available.names[count.index]
 }
 
 # NAT Gateway
